d304cdea4366da2c74bc62ff9fc62f1dd512173f
user <user@192.168.1.18>
2023-08-27 14:54:21 +0530

    first commit

---
+++ Sana/src/test/java/Sana/Sana/Sana1.java
@@ -0,0 +1,18 @@
+package Sana.Sana;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+public class Sana1 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver=new InternetExplorerDriver();
+		driver.get("https://www.facebook.com/");
+		
+		
+	}
+
+}
